b6598321376f817495ffdd43e6bc9bde68c23674
FanonX Rogers <fanonxdevelops@gmail.com>
2020-04-05 19:58:02 -0400

    Update Activity model data

--- models/activitymodel.go
+++ models/activitymodel.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"github.com/globalsign/mgo/bson"
-	"log"
 )
 // SensorData interface that each type of sensor model will implement to interact with the database
 type SensorData interface {
@@ -22,39 +21,7 @@ type ActivitySensorData struct {
 	Activity int `json:"activity,omitempty" bson:"activity,omitempty"`
 	Duration float32 `json:"duration,omitempty" bson:"duration,omitempty"`
 }
-// How the activity model will be structured
-type ActivityModel struct {}
 
-// handle inserting data to the db
-func (activityData *ActivityModel) InsertActivityData(data ActivitySensorData) error {
-	// connect to the activity connection
-	collection := dbConnect.Use(databaseName, "activity")
-	// assign result to error object while saving activity data
-	err := collection.Insert(bson.M{
-		"starttime": data.StartTime,
-		"endtime": data.EndTime,
-		"activity": data.Activity,
-		"duration": data.Duration,
-	})
-
-	return err
-}
-
-// Handle getting all documents from activity collection
-func (activityData *ActivityModel) FetchAllActivityData() []ActivitySensorData {
-	var result []ActivitySensorData
-	// connect to the activity collection
-	collection := dbConnect.Use(databaseName, "activity")
-
-	// fetch all the documents within that database
-	err := collection.Find(nil).All(&result)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return result
-}
 
 
 
